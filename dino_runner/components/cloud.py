e2ff870a66a26616e5f0666fd3ecbaa5479fcc54
GiAyBiOu <gabrielmendoza4u@gmail.com>
2022-10-04 16:37:07 -0400

    modificaciones

---
+++ dino_runner/components/cloud.py
@@ -0,0 +1,13 @@
+from pygame.sprite import Sprite
+from utils.constants import CLOUD
+
+class Cloud(Sprite):
+    
+    def __init__(self):
+        self.image = CLOUD
+        self.cloud_rect = self.image.get_rect()
+        self.cloud_rect.x = 400
+        self.cloud_rect.y = 200
+    
+    def draw(self, screen):
+        screen.blit(self.image, (self.cloud_rect.x, self.cloud_rect.y)) #Blit recibe un objeto tupla, para que se dibuje nuestro objeto
